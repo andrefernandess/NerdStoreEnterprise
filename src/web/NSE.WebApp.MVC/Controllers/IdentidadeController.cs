ec7a88dfc9dbb861b92cbe6ac52578fd278938fd
andre fernandes <andre.l.fernandess@hotmail.com>
2021-05-23 22:20:00 -0300

    Add project files.

---
+++ src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NSE.WebApp.MVC.Models;
+using NSE.WebApp.MVC.Services;
+
+namespace NSE.WebApp.MVC.Controllers
+{
+    public class IdentidadeController : Controller
+    {
+        private readonly IAuthenticationService _authenticationService;
+
+        public IdentidadeController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return View(usuarioRegistro);
+
+            //API
+
+            if (false) return View(usuarioRegistro);
+
+            //Realizar Login
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid) return View(usuarioLogin);
+
+            var respostaLogin = await _authenticationService.Login(usuarioLogin);
+
+            if (false) return View(usuarioLogin);
+
+            //Realizar Login
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("sair")]
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
---
+++ src/web/NSE.WebApp.MVC/Services/AuthenticationService.cs
@@ -0,0 +1,52 @@
+﻿using NSE.WebApp.MVC.Models;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly HttpClient _httpClient;
+
+        public AuthenticationService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<UsuarioRespostaLogin> Login(UsuarioLogin usuarioLogin)
+        {
+            var loginContent = new StringContent(
+                JsonSerializer.Serialize(usuarioLogin),
+                Encoding.UTF8,
+                "application/json"
+            );
+
+            var response = await _httpClient.PostAsync("https://localhost:44341/api/identidade/autenticar", loginContent);
+
+            var teste = await response.Content.ReadAsStringAsync();
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+
+            return JsonSerializer.Deserialize<UsuarioRespostaLogin>(await response.Content.ReadAsStringAsync());
+        }
+
+        public async Task<UsuarioRespostaLogin> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            var registroContent = new StringContent(
+                JsonSerializer.Serialize(usuarioRegistro),
+                Encoding.UTF8,
+                "application/json"
+            );
+
+            var response = await _httpClient.PostAsync("https://localhost:44341/api/identidade/nova-conta", registroContent);
+
+            return JsonSerializer.Deserialize<UsuarioRespostaLogin>(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
