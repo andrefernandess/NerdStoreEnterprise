0972dc2bbc3aba5c7059d1959fb14288056384d2
andre fernandes <andre.l.fernandess@hotmail.com>
2021-09-20 23:23:14 -0300

    Ajustado arquivos

--- src/services/NSE.Identidade.API/NSE.Identidade.API/Models/UsuarioLogin.cs
+++ src/services/NSE.Identidade.API/NSE.Identidade.API/Models/UsuarioLogin.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace NSE.Identidade.API.Models
 {
-    public class UsuarioRegistro
+    public class UsuarioLogin
     {
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [EmailAddress(ErrorMessage = "O campo {0} esta em formato inválido")]
@@ -14,8 +11,5 @@ namespace NSE.Identidade.API.Models
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(100, ErrorMessage = " O campo {0} deve ter entre {1} e {2} caracteres", MinimumLength = 6)]
         public string Senha { get; set; }
-
-        [Compare("Senha", ErrorMessage = "As senhas não conferem.")]
-        public string SenhaConfirmacao { get; set; }
     }
 }
